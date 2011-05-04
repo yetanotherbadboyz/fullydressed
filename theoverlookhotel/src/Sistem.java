8e4ebe81f2e944fa3309cc1ca4dc2bdffd87307f
Mesutcan Kurt <mesutcank@gmail.com>
2011-05-04 18:02:08 +0300

    modify sistem.java

--- theoverlookhotel/src/Sistem.java
+++ theoverlookhotel/src/Sistem.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Sistem {
@@ -70,10 +71,64 @@ public class Sistem {
 
 	}
 	
+	public void anaMenuYazdir() {
+		System.out.println("-------TheOverLookHotel-------\n" +
+				"\n1. Sistem'e Resepsiyonist olarak giriş yap. " +
+				"\n2. Sistem'e Yönetici olarak giriş yap." +
+				"\n\n\n0. Çıkış." +
+				"\n\n-------TheOverLookHotel-------\nSeçiminiz: ");
+	}
+	
+	private static void resepsiyonistMenuYazdir() {
+		System.out.println("-------TheOverLookHotel-------\n" +
+				"=======Resepsiyonist Menu=======\n" +
+				"\n1. Müşteri Kayıt işlemi. " +
+				"\n2. Müşteri Listeleme işlemi." +
+				"\n3. Oda Kiralama İşlemi." +
+				"\n4. Boş Oda Listeleme İşlemi" +
+				"\n\n\n9. Ana Menü'ye geri dön." +
+				"\n0. Sistemden Çıkış." +
+				"\n\n-------TheOverLookHotel-------\nSeçiminiz: ");
+
+	}
+	
+	private static void yoneticiMenuYazdir() {
+		System.out.println("-------TheOverLookHotel-------\n" +
+				"=======Yönetici Menü=======\n" +
+				"\n1. Müşteri Kayıt işlemi. " +
+				"\n2. Müşteri Listeleme işlemi." +
+				"\n3. Oda Kiralama İşlemi." +
+				"\n4. Boş Oda Listeleme İşlemi" +
+				"\n5. Yeni Oda Kayıt işlemi." +
+				"\n6. Yeni Resepsiyonist Kayıt işlemi." +
+				"\n\n\n9. Ana Menü'ye geri dön." +
+				"\n0. Sistemden Çıkış." +
+				"\n\n-------TheOverLookHotel-------\nSeçiminiz: ");
+
+	}
+	
 	
 	public static void main(String[] args) {
-		
-		
+		Sistem sistem = new Sistem();
+		Scanner in = new Scanner(System.in);
+		int gelen = -1;
+		while (gelen != 0){
+			sistem.anaMenuYazdir();
+			gelen = in.nextInt();
+			if (gelen == 1)
+				resepsiyonistMenuYazdir();
+				
+			
+			else if (gelen == 2)
+				yoneticiMenuYazdir();
+			
+			else if (gelen == 0)
+				System.exit(0);
+			
+			else {
+				System.out.println("\n\nYanlis Secim.\n\n");
+			}
+		}
 	}
 
 }
